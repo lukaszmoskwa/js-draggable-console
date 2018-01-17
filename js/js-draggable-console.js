56554868621be2444c07a853df3bbd7bf19a191b
Lykos94 <lukaszmoskwa94@gmail.com>
2018-01-18 00:45:24 +0100

    Added few functions and input

--- js/js-draggable-console.js
+++ js/js-draggable-console.js
@@ -15,8 +15,8 @@
  * Js-Draggable-Console
  */
 
-var drag_console = `<div id="js-draggable-console" style="height: 70vh; width: 80vw; background-color: rgba(0,0,0,0.7); border: 4px solid #0060FF; position: fixed; top: 0; bottom: 0; left: 0; right: 0; margin: auto; overflow-y: scroll; display: none"><div style='width: 100%; background-color: #0060FF; color: white;'><h4 style='margin:0'>Js-Draggable-Console</h4></div></div>`;
-var drag_button = `<div id="js-draggable-button" style="height: 40px; width: 40px; background-color: white; border: 5px solid #0060FF; z-index: 999;  border-radius: 25px;"></div>`;
+var drag_console = `<div id="js-draggable-console" style="height: 70vh; width: 80vw; background-color: rgba(0,0,0,0.7); border: 4px solid #0060FF; position: fixed; top: 0; bottom: 0; left: 0; right: 0; margin: auto; overflow-y: scroll; display: none"><div style='width: 100%; background-color: #0060FF; color: white;'><h4 style='margin:0; padding-top:5px; padding-bottom:5px; padding-left:5px;'>Js-Draggable-Console</h4></div><input type='text' id='js-input-text' style='width:100%; position:absolute; bottom:0;'></div>`;
+var drag_button = `<div id="js-draggable-button" style="height: 40px; width: 40px; background-color: white; border: 5px solid #0060FF; z-index: 999;  border-radius: 25px; left:50%;"></div>`;
 $("body").append(drag_console);
 $("body").append(drag_button);
 $("#js-draggable-button").draggable();
@@ -26,12 +26,51 @@ $("#js-draggable-button").on("click", function(){
 	$("#js-draggable-console").toggle("drop", 500);
 });
 
+$("body").append("<style>.jdc-message{margin:0}</style>");
+
+
 var JDC = {
-	"alert" : ""
+	"log" : "",
+	"warn" : "",
+	"info" : "",
+	"error" : "",
+	"ajax" : ""
 }
-JDC.alert = function(param){
-	$("#js-draggable-console").append("<p style='color:white;'>" + param + "</p>");
+JDC.log = function(param){
+	$("#js-draggable-console").append("<p class='jdc-message' style='color:black;background-color: #7CFC00;'>LOG ->" + param + "</p>");
+};
+JDC.warn = function(param){
+	$("#js-draggable-console").append("<p class='jdc-message' style='color:black; background-color:yellow'>WARNING -> " + param + "</p>");
+};
+JDC.error = function(param){
+	$("#js-draggable-console").append("<p class='jdc-message' style='color:black; background-color:red'>ERROR ->" + param + "</p>");
+};
+JDC.info = function(param){
+	$("#js-draggable-console").append("<p class='jdc-message' style='color:white;'>INFO ->" + param + "</p>");
+};
+JDC.ajax = function(param){
+	$("#js-draggable-console").append("<p class='jdc-message' style='color:gray; background-color:black'>AJAX error -> " + param + "</p>");
 };
 
+var console = {};
+console.log = function(param){
+	JDC.log(param);
+};
+console.warn = function(param){
+	JDC.warn(param);
+};
+console.error = function(param){
+	JDC.error(param);
+};
+console.info = function(param){
+	JDC.info(param);
+};
 
-JDC.alert("ok");
+window.console = console;
+
+$('input').on('keydown', function(e){
+    if(e.which === 9 || e.which === 13) {
+        var command = $("#js-input-text").val();
+        eval(command);
+    }
+});
